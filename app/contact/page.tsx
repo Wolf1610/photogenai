2001a63e4d263404d8a57bfd73c63accae836eab
Wolf1610 <dutta2772004@gmail.com>
2025-04-06 13:15:29 +0530

    updated contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -46,16 +46,16 @@ export default function NatureGallery() {
             <img src="https://i.pinimg.com/474x/07/17/95/071795ba58e2cd600395cf8ecdaf10d1.jpg" className="w-[150px] h-[150px] bg-[#ddd] rounded-full mx-auto mb-4"/>
           </div>
           <h3 className="text-[#1a2a6c] mb-2 font-semibold text-lg">Sachin kumar Kanshi</h3>
-          <p className="text-gray-600 mb-2">RegdNo-230301120400</p>
-          <p className="text-gray-600 mb-2">230301120400@centurionuniv.edu.in</p>
+          <p className="text-gray-600 mb-2">RegdNo-230301120384</p>
+          <p className="text-gray-600 mb-2">230301120384@centurionuniv.edu.in</p>
         </div>
         <div className="text-center p-2 bg-gray-100 rounded-[10px] transition-transform duration-300 ease-in-out hover:-translate-y-1">
           <div className="w-[150px] h-[150px] bg-[#ddd] rounded-full mx-auto mb-4 object-cover">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmn6hmw_OQg5ZWjtllDflJb1MoIszq1-j4HQ&s" alt="Ddebanshu" className="w-[150px] h-[150px] bg-[#ddd] rounded-full mx-auto mb-4"/>
           </div>
-          <h3 className="text-[#1a2a6c] mb-2 font-semibold text-lg">Devanshu</h3>
-          <p className="text-gray-600 mb-2">RegdNo-230301120400</p>
-          <p className="text-gray-600 mb-2">230301120400@centurionuniv.edu.in</p>
+          <h3 className="text-[#1a2a6c] mb-2 font-semibold text-lg">Devanshu Das</h3>
+          <p className="text-gray-600 mb-2">RegdNo-230301120418</p>
+          <p className="text-gray-600 mb-2">230301120418@centurionuniv.edu.in</p>
         </div>
       </div>
     </section>
